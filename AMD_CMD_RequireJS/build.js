a84c18c5c8a73d30b243786b5d3ddf494ffa021f
1720966051@qq.com <1720966051@qq.com>
2017-12-13 03:03:07 +0800

    r.js打包

---
+++ AMD_CMD_RequireJS/build.js
@@ -0,0 +1,14 @@
+/*
+* @Author: Administrator
+* @Date:   2017-12-13 02:48:45
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-12-13 02:49:30
+*/
+({
+    baseUrl: "./src/js",
+    paths: {
+        jquery: "lib/jquery"
+    },
+    name: "main",
+    out: "dest/dest-min.js"
+})
